a9b0dc56e4a8195edcffd5ce34cb61571dfefecb
Oscar Busk <oscar.busk@gmail.com>
2018-11-05 02:14:59 +0100

    Update enviroment

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,37 @@
+'use strict';
+const semver = require('semver');
+
+function getSupportedTypescriptTarget() {
+    if (process.argv.includes('--debuggable')) {
+        // If run with `--debuggable` we run in the latest available TS target to make asynchrounous methods steppable
+
+        const nodeVersion = process.versions.node;
+
+        if (semver.gte(nodeVersion, '10.0.0')) {
+            return 'es2018';
+        } else if (semver.gte(nodeVersion, '7.6.0')) {
+            return 'es2017';
+        } else if (semver.gte(nodeVersion, '7.0.0')) {
+            return 'es2016';
+        }
+    }
+
+    return 'es2015';
+}
+
 module.exports = {
-  testURL: 'http://localhost',
-  transform: {
-    '.(tsx?)': 'ts-jest'
-  },
-  testMatch: [
-    '**/__tests__/**/*.{t,j}s?(x)',
-    '**/?(*.)(spec|test).{t,j}s?(x)'
-  ],
-  testPathIgnorePatterns: [
-    '<rootDir>/(node_modules|lib|es|dist)'
-  ],
-  collectCoverageFrom: [
-    'src/**/*.{t,j}s?(x)',
-    '!src/**/*.d.ts',
-  ],
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
-  globals: {
-    "ts-jest": {
-      babelConfig: undefined,
-    },
-  },
+    testEnvironment: 'node',
+    testURL: 'http://localhost',
+    preset: 'ts-jest',
+    collectCoverageFrom: [
+        'src/**/*.{t,j}s?(x)',
+        '!src/**/*.d.ts',
+    ],
+    globals: {
+        'ts-jest': {
+            tsConfig: {
+                target: getSupportedTypescriptTarget(),
+            }
+        }
+    }
 };
