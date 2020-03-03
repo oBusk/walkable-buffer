443064ee18f96c6dbe6c58a08a7a34a795c823ff
Oscar Busk <oscar.busk@gmail.com>
2020-03-03 20:46:26 +0100

    style: allow explicit any in test files

--- src/Encoding.spec.ts
+++ src/Encoding.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { isEncoding } from "./Encoding";
 
 describe("isEncoding", () => {
--- src/Endianness.spec.ts
+++ src/Endianness.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { isEndianness } from "./Endianness";
 
 describe("isEndianness", () => {
